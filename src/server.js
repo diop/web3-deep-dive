f49233da91e7be7524e4a9fb5f2d2ba7de5ab858
diop <fodesdiop@gmail.com>
2017-11-13 15:31:51 -0800

    Jason's Patches

--- src/server.js
+++ src/server.js
@@ -13,14 +13,70 @@ app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(bodyParser.json())
+
+const functionNames = [
+  'eth.getGasPrice',
+  'eth.getAccounts',
+  'eth.getBlockNumber',
+  'eth.net.getId',
+  'eth.net.isListening',
+  'eth.net.getPeerCount',
+  'eth.accounts.hashMessage',
+  'utils.randomHex',
+  'utils.sha3',
+  'utils.keccak256',
+  'utils.isHex',
+  'utils.isHexStrict',
+  'utils.isAddress',
+  'utils.toChecksumAddress',
+  'utils.checkAddressChecksum',
+  'utils.toHex',
+  'utils.hexToNumberString',
+  'utils.hexToNumber',
+  'utils.numberToHex',
+  'utils.hexToUtf8',
+  'utils.hexToAscii',
+  'utils.utf8ToHex',
+  'utils.asciiToHex',
+  'utils.hexToBytes',
+  'utils.bytesToHex',
+  'utils.toWei',
+  'utils.fromWei',
+  'utils.toWei',
+  'utils.unitMap',
+  'utils.padLeft',
+  'utils.padRight',
+  'utils.toTwosComplement',
+]
+
 app.get('/', (request, response) => {
-  response.sendFile(__dirname + '/public/index.html')
+  response.render('index', {functionNames})
 })
 
-app.get('/ajax-request', (request, response) => {
-  web3.eth.getAccounts((error, addresses) => {
-    if (!error) {response.json(addresses)}
-  })
+app.post('/ajax-request', (request, response) => {
+  const { functionName, parameters } = request.body
+
+  console.log('server receiving ajax-request')
+  console.log('    functionName:', functionName)
+  console.log('    parameters:', parameters)
+
+  const theFunction = functionName.split('.')
+    .reduce((acc, part) => acc[part], web3)
+
+  if (!(theFunction instanceof Function)) {
+    response.send(JSON.stringify(theFunction))
+  } else {
+    const result = theFunction.apply(null, JSON.parse(parameters))
+
+    if (result.then) {
+      result.then(promiseResult => {
+        response.send(JSON.stringify(promiseResult))
+      })
+    } else {
+      response.send(JSON.stringify(result))
+    }
+  }
 })
 
 app.listen(port, () => {
