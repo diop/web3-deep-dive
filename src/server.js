970f68b596b15c75a1d4dc7932b9a78f84ec4ca0
diop <fodesdiop@gmail.com>
2017-11-14 11:10:27 -0800

    simplify server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,11 @@
 const express = require('express')
 const app = express()
-const ejs = require('ejs')
 const path = require('path')
 const bodyParser = require('body-parser')
-const session = require('express-session')
 const Web3 = require('web3')
 const web3 = new Web3('http://localhost:8545')
 const port = process.env.PORT || 3000
 
-app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static(path.join(__dirname, 'public')))
